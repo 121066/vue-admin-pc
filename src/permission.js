b1bb9e5205a3bc4f4f56b889647c00558e6e31bd
w_dengbin <1318720440@qq.com>
2022-08-01 14:13:00 +0800

    添加登录路由判断

--- src/permission.js
+++ src/permission.js
@@ -1,9 +1,18 @@
 import router from './router'
 import store from './store'
 import { getCookies } from '@/utils/cookies'
+const whiteList = ['/login', '/auth-redirect']
 router.beforeEach(async (to, from, next) => {
-  if (to.path === '/login') return next()
-  const tokenStr = getCookies('web-token')
-  if (!tokenStr) return next('/login')
-  next()
+  if (getCookies()) {
+    if (to.path === '/login') return next({ path: '/' })
+    const tokenStr = getCookies('web-token')
+    if (!tokenStr) return next('/login')
+    next()
+  } else {
+    if (whiteList.indexOf(to.path) !== -1) {
+      next()
+    } else {
+      next(`/login?redirect=${to.path}`)
+    }
+  }
 })
